a1561e8b63c6eeb33c772698817383c0f00a793e
EDA <eda@mac-mini-3.local>
2017-01-20 12:51:27 +1300

    add button

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
 const Grid = ({props}) => (
-  <div className='Grid'>
-    
+  <div className="container">
+    <table className="table">
+      <tbody>
+        <tr className="track">
+          <th className="row">Synth</th>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+        </tr>
+        <tr className="track">
+          <th className="row">Kick</th>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+          <td><button type="button" className="btn btn-outline-primary"></button></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 )
 
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,53 @@
 import React from 'react'
 
+import Controller from '../components/Controller'
+import Grid from '../components/Grid'
+import Header from '../components/Header'
+
+const NUM_STEPS = 8
+
+function createThread (name, sound) {
+  const steps = new Array(NUM_STEPS)
+  return {
+    name,
+    sound,
+    steps
+  }
+}
+
 export default React.createClass({
+
+  getInitialState() {
+
+    const threads = [
+      {
+        name: 'Perc126-01',
+        sound: 'Perc126-01.wav',
+        // sound: new Audio('Perc126-01.wav'),
+        steps: [true, false, true, false, true, false, true, false]
+      },
+      {
+        name: 'PercFX126-01',
+        sound: 'PercFX126-01.wav',
+        // sound: new Audio('PercFX126-01.wav'),
+        steps: [false, false, false, false, false, false, false, false]
+      }
+    ]
+
+    return {
+      threads
+    }
+  },
+
   render () {
+
+    const threads = this.state.threads
+
     return (
       <div>
-        <h1>App</h1>
+        <Header />
+        <Grid threads={threads} />
+        <Controller />
       </div>
     )
   }
