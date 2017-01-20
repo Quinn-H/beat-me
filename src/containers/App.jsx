15a492b9171987eb3edc81b2fe829ca07d788804
EDA <eda@mac-mini-1.local>
2017-01-20 14:43:32 +1300

    Sounds are working

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -27,6 +27,28 @@ function toggleStep (state, trackIndex, stepIndex) {
 
 const App = React.createClass({
 
+  beat () {
+    const numSteps = this.state.numSteps
+    const currentStep = this.state.currentStep
+    const tracks = this.state.tracks
+
+    console.log('step:', currentStep)
+
+    for (let i = 0; i < tracks.length; i++) {
+      const track = tracks[i]
+      if (track.steps[currentStep].isSet) {
+        // track.sound.currentTime = 0
+        // track.sound.play()
+        new Audio(track.sound).play()
+      }
+    }
+
+    const nextStep = currentStep === numSteps - 1 ? 0 : currentStep + 1
+    const newState = Object.assign({}, this.state)
+    newState.currentStep = nextStep
+    this.setState(newState)
+  },
+
   getInitialState () {
     // const stepTime = 1000 * 60 / bpm
     const bpm = 100
@@ -48,6 +70,10 @@ const App = React.createClass({
     }
   },
 
+  isPlaying () {
+    return this.state.intervalId !== null
+  },
+
   render () {
     const tracks = this.state.tracks.map((track, trackIndex) => {
       return {
@@ -66,55 +92,41 @@ const App = React.createClass({
         })
       }
     })
-    const isPlaying = false
-    const start = function () {}
-    const stop = function () {}
+    const isPlaying = this.isPlaying()
+    const start = this.start
+    const stop = this.stop
     return (
       <div>
         <Header />
-        <Grid threads={tracks} />
+        <Grid threads={tracks} tracks={tracks} />
+        <button onClick={start}>Start</button>
+        <button onClick={stop}>Stop</button>
         <Controller isPlaying={isPlaying} start={start} stop={stop}/>
       </div>
     )
   },
 
   start () {
-    intervalId = setInterval(beat, stepTime)
+    const stepTime = 1000 * 60 / this.state.bpm
+    const newState = Object.assign({}, this.state)
+    newState.intervalId = setInterval(this.beat, stepTime)
+    this.setState(newState)
   },
 
   stop () {
-    clearInterval(intervalId)
-    intervalId = null
+    clearInterval(this.state.intervalId)
+    const newState = Object.assign({}, this.state)
+    newState.intervalId = null
+    this.setState(newState)
   }
 
 })
-
-
-
-function beat () {
-  console.log('step:', currentStep)
-  currentStep++
-  if (currentStep >= numSteps)
-    currentStep = 0
-  for (let i = 0; i < tracks.length; i++) {
-    let track = tracks[i]
-    if (track.steps[currentStep]) {
-      // track.sound.currentTime = 0
-      // track.sound.play()
-      new Audio(track.sound).play()
-    }
-  }
-}
 
 function clickStopStart () {
   if (isPlaying())
     stop()
   else
     start()
-}
-
-function isPlaying () {
-  return intervalId !== null
 }
 
 export default App
