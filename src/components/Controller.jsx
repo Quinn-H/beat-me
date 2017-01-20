c596839cebc9dde1e6e6a1d0ddfa2cbd8de4a799
EDA <eda@mac-mini-3.local>
2017-01-20 14:44:44 +1300

    update controller

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,25 +1,14 @@
 import React from 'react'
 
-const Controller = ({ }) => (
-  <div className='controller'>
-    <h1>This is Controller</h1>
-  </div>
+const Controller = (props) => {
+  const controller = props.controller
+  const onClick = controller.onClick
+  return(
+  <td>
+    <button type="button" className="btn btn-outline-danger" onClick={onClick}>
+    </button>
+  </td>
 )
+}
 
 export default Controller
-
-
-// import React from 'react'
-//
-// const Controller = (props) => {
-//   const step = props.step
-//   const onClick = step.onClick
-//   return (
-//     <td>
-//       <button type="button" className="btn btn-outline-primary" onClick={onClick}>
-//       </button>
-//     </td>
-//   )
-// }
-//
-// export default Controller
